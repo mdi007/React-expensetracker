6b5d63554fe5f8522ac8128446fe80b3dbbc27af
MOHAMMAD IBRAHIM <86375732+mdi007@users.noreply.github.com>
2022-10-27 19:40:44 +0530

    ReactApp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+import NewExpense from "./components/NewExpense/NewExpense";
+
+import Expenses from "./components/Expenses/Expenses";
+
+let DUMMY_EXPENSE = [];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSE);
+
+  function fetchData() {
+    fetch("https://techgun.website/sample/api/read.php")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        //console.log(data);
+        setExpenses(data);
+      });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const addExpenseHandler = (expense) => {
+    // fetch('https://techgun.website/sample/api/create.php', {
+    //     method: 'POST',
+    //     body: JSON.stringify(expense),
+    //     headers: {
+    //         'content-Type' : 'application/json'
+    //     }
+    // }).then(
+    //     response => {
+    //         fetchData();
+    //     }
+    // );
+    const updatedExpense = [expense, ...expenses];
+    setExpenses(updatedExpense);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses item={expenses} />
     </div>
   );
-}
+};
 
 export default App;
